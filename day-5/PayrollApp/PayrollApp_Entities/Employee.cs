3b2d480d36205a316c8bf8937025649720eb90b0
Sayeedul Rahman <sayeedulrh.sr@gmail.com>
2025-05-12 21:49:04 +0530

    Adding on day 6

---
+++ day-5/PayrollApp/PayrollApp_Entities/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+
+namespace PayrollApp.Entities
+{
+    public class Employee
+    {
+        protected readonly int id; // should be assigned ONLY through constructor
+        protected string name;
+        protected decimal basicPay;
+        protected decimal daPay;
+        protected decimal hraPay;
+        //protected decimal totalPay;  // will be set by a method, will NOT be passed from outside the class
+
+
+        public Employee() { }
+
+        public Employee(int id, string name, decimal basicPay, decimal daPay, decimal hraPay)
+        {
+            this.id = id;
+            this.name = name;
+            this.basicPay = basicPay;
+            this.daPay = daPay;
+            this.hraPay = hraPay;
+        }
+
+        public int Id
+        {
+            get { return this.id; } //readOnly
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public decimal BasicPay
+        {
+            get { return this.basicPay; }
+            set { this.basicPay = value; }
+        }
+        public decimal DaPay
+        {
+            get { return this.daPay; }
+            set { this.daPay = value; }
+        }
+        public decimal HraPay
+        {
+            get { return this.hraPay; }
+            set { this.hraPay = value; }
+        }
+
+        //public decimal TotalPay
+        //{
+        //    get { return this.totalPay; } //readOnly 
+        //}
+
+        public virtual void CalculateSalary() { }
+
+    }
+}
---
+++ day-6/PayrollApp/PayrollApp_Entities/Hr.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollApp.Entities
+{
+    public class Hr : Employee
+    {
+        /*
+         * -- Below data members inherited from the Parent class Employee
+        readonly int id;  should be assigned ONLY through constructor
+        string name;
+        decimal basicPay;
+        decimal daPay;
+        decimal hraPay;
+         */
+        decimal gratuityPay;
+        decimal totalPay;  // will be set by a method, will NOT be passed from outside the class
+
+        public Hr() { }
+
+        public Hr(int id, string name, decimal basicPay, decimal daPay, decimal hraPay,
+            decimal gratuityPay) : base(id,name,basicPay,daPay,hraPay)
+        {
+            this.gratuityPay = gratuityPay;
+            
+        }
+        
+        public decimal TotalPay 
+        {
+            get { return this.totalPay; } //readOnly 
+        }
+
+        public decimal GratuityPay
+        {
+            get { return this.gratuityPay;  }
+            set { this.gratuityPay = value; }
+        }
+
+        public override void CalculateSalary() // overriding the Parent Virtual Function
+        {
+            this.totalPay = base.basicPay + base.daPay + base.hraPay + this.gratuityPay;
+        }
+    }
+}
